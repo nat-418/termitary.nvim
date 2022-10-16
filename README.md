df24ef2b6ce06c8d1953f285e9ef3a6edb77b88c
nat-418 <93013864+nat-418@users.noreply.github.com>
2022-10-16 15:21:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+termitary.nvim
+==============
+
+Termitary is a simple Neovim plugin for interacting with terminal buffers.
+
+Usage
+-----
+
+Termitary ships a single `:Termitary` command with a few subcommands:
+
+| subcommand name  | action                                                   |
+| ---------------- | -------------------------------------------------------- |
+| `activate`       | Set the current terminal buffer to work with Termitary   |
+| `execute {text}` | Send some `{text}` to the active terminal with a newline |
+| `new`            | Open a new terminal buffer and `activate` it             |
+| `paste`          | Send contents of the `"` register to the terminal buffer |
+| `repeat`         | Emulate pressing `<Up><CR>` in the terminal buffer       |
+| `{range}send`    | Put some `{range}` of the current buffer in the terminal |
+
+For example, to send the contents of some buffer to a REPL running in the
+active terminal buffer: `:%Termitary send`.
+
+Installation
+------------
+
+Using [Packer](https://github.com/wbthomason/packer.nvim):
+```lua
+use({
+  'https://github.com/nat-418/termitary.nvim',
+  config = function()
+    require('termitary').setup()
+  end
+})
+```
+
+Configuration
+-------------
+
+The default command name is `Termitary` but you can change it by setting
+the `command_name` option in the `setup()` function, for example to `:T`:
+
+```
+require('termitary').setup({
+  command_name = 'T'
+})
+```
---
+++ lua/termitary.lua
@@ -0,0 +1,95 @@
+local M = {}
+
+M.state = {
+  terminal_id = nil
+}
+
+M.run = function(args)
+  if args == nil then return false end
+
+  -- Warning: mutating fargs table
+  local subcommand = table.remove(args.fargs, 1)
+
+  if subcommand == nil then return false end
+
+  if subcommand == 'activate' then
+    M.state.terminal_id = vim.b.terminal_job_id
+
+    if M.state.terminal_id == nil then
+      print('Error: not in a terminal buffer')
+      return false
+    end
+
+    return true
+  end
+
+  if subcommand == 'new' then
+    vim.api.nvim_command('terminal')
+    M.state.terminal_id = vim.b.terminal_job_id
+    return true
+  end
+
+  -- The following subcommands require a terminal_id
+  if M.state.terminal_id == nil then
+    print('Error: no terminal is active.')
+    return false
+  end
+
+  if subcommand == 'execute' then
+    return vim.api.nvim_chan_send(M.state.terminal_id, args.args .. '\r')
+  end
+
+  if subcommand == 'repeat' then
+    -- Send Up arrow code
+    return vim.api.nvim_chan_send(M.state.terminal_id, '\x1b\x5b\x41\r')
+  end
+
+  if subcommand == 'send' then
+    local selection = vim.api.nvim_buf_get_lines(
+      0,
+      args.line1 - 1,
+      args.line2,
+      {}
+    )
+
+    for _, line in ipairs(selection) do
+      vim.api.nvim_chan_send(M.state.terminal_id, (line .. '\r'))
+    end
+
+    return true
+  end
+
+  if subcommand == 'paste' then
+    return vim.api.nvim_chan_send(
+      M.state.terminal_id,
+      vim.fn.getreg('"')
+    )
+  end
+
+  return false
+end
+
+M.setup = function(opts)
+  local completion = function()
+    return {
+      'activate',
+      'execute',
+      'new',
+      'paste',
+      'repeat',
+      'send'
+    }
+  end
+
+  if opts.command_name == nil then
+    opts.command_name = 'Termitary'
+  end
+
+  return vim.api.nvim_create_user_command(
+    opts.command_name,
+    function(args) M.run(args) end,
+    {nargs = '*', complete = completion, range = true}
+  )
+end
+
+return M
